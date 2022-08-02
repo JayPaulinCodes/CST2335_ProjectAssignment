6fea15c54928e5ef5d66e98dbe480fe644547314
JayPaulinCodes <jacobwpaulin@gmail.com>
2022-08-01 23:15:51 -0400

    Added a fetchFromAPI method to TicketQuery

--- app/src/main/java/com/cst2335/projectassignment/utils/TicketQuery.java
+++ app/src/main/java/com/cst2335/projectassignment/utils/TicketQuery.java
@@ -1,10 +1,29 @@
 package com.cst2335.projectassignment.utils;
 
+import org.json.JSONObject;
+
 // TODO: Add JavaDoc Comment
 public class TicketQuery {
 
     public static final String ACTIVITY_HOME = "activityPage_home";
     public static final String ACTIVITY_SEARCH = "activityPage_search";
     public static final String ACTIVITY_FAVORITES = "activityPage_favorites";
+
+    public static final JSONObject fetchFromAPI(String city, Integer radius) {
+        HTTPRequest httpRequest = new HTTPRequest();
+        httpRequest.execute(httpRequest.url(city, radius));
+
+        String result = null;
+        JSONObject resultJSON = null;
+        try {
+            result = httpRequest.get();
+            resultJSON = new JSONObject(result);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            resultJSON = null;
+        }
+
+        return resultJSON;
+    }
 
 }
