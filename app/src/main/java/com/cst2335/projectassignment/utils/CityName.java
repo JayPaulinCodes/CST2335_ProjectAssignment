6d5cdf197591c5e2ca0f82f6fb6edc76c163de34
JayPaulinCodes <jacobwpaulin@gmail.com>
2022-08-01 23:19:23 -0400

    Created a class to get the city name of the device's location

---
+++ app/src/main/java/com/cst2335/projectassignment/utils/CityName.java
@@ -0,0 +1,37 @@
+package com.cst2335.projectassignment.utils;
+
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.AsyncTask;
+
+import com.cst2335.projectassignment.activities.JActivity;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class CityName extends AsyncTask<Location, Integer, List<Address>> {
+
+    private Context context;
+
+    public CityName(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    protected List<Address> doInBackground(Location... locations) {
+        Location location = locations[0];
+
+        Geocoder coder = new Geocoder(context, Locale.ENGLISH);
+        List<Address> results = null;
+
+        try {
+            results = coder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+        } catch (IOException e) {
+            // nothing
+        }
+        return results;
+    }
+}
